25ff47a09251b3d559c50675bf8c6d24a6b7d141
RAJKUMAR PRAJAPATI <160745382+rajkumarprajapti@users.noreply.github.com>
2024-12-23 07:56:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a row-wise sorted matrix/search-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t > 0) {
+            int rows = sc.nextInt();
+            int columns = sc.nextInt();
+
+            int matrix[][] = new int[rows][columns];
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            int target = sc.nextInt();
+
+            Solution x = new Solution();
+
+            if (x.searchRowMatrix(matrix, target))
+                System.out.println("true");
+            else
+                System.out.println("false");
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchRowMatrix(int[][] mat, int x) {
+        // code here
+         // code here
+        int r,c;
+        
+        r=mat.length;
+        c=mat[0].length;
+        
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(mat[i][j]==x)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
